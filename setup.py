3955fe46ef4f06231f1c52ba3fe459f91f170bca
Jeff Balogh <jbalogh@mozilla.com>
2011-05-17 15:18:07 -0700

    VENDING MACHINE

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='vend',
+    license='BSD',
+    version='0.0.1',
+    description='a script for managing your vendor library',
+    author='Jeff Balogh',
+    url='https://github.com/jbalogh/vend',
+    py_modules=['argparse'],
+    scripts=['vend'],
+)
